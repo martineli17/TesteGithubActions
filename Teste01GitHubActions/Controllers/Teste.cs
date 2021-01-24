09a56300c3e764b3022c01ff00318a62ec0e8225
Fabio Martineli <fabio17lmg@outlook.com>
2021-01-24 19:47:36 -0300

    Add project files.

---
+++ Teste01GitHubActions/Controllers/Teste.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Teste01GitHubActions.Controllers
+{
+    [ApiController]
+    [Route("teste")]
+    public class Teste : Controller
+    {
+        private readonly IConfiguration configuration;
+        public Teste(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IActionResult Index() => Ok("Versão 1.0");
+        [HttpGet("ambiente")]
+        public IActionResult Ambiente() => Ok(configuration.GetSection("ambiente").Value);
+    }
+}
---
+++ Testes/Teste1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Testes
+{
+    public class Teste1
+    {
+        [Fact(DisplayName = "01")]
+        [Trait("Teste", "01")]
+        public void Teste01()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "02")]
+        [Trait("Teste", "02")]
+        public void Teste02()
+        {
+            Assert.True(false);
+        }
+    }
+}
